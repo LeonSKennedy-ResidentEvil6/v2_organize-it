f507e80a052d86f65383f85321980ca849f8ee7c
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2021-02-24 09:52:11 -0800

    testing organize it version code

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,10 +1,114 @@
-document.addEventListener('DOMContentLoaded', () => {
-    getEvent()
-});
+const eventEndPoint = "http://127.0.0.1:3000/events"
+const participantEndPoint = "http://127.0.0.1:3000/participants"
 
-async function getEvent() {
-    fetch("http://127.0.0.1:3000/events")
-        .then(response => response.json())
-        .then(result => console.log(result))
-        .catch((error) => alert(error.message))
+// Load forms and other content to app html page
+document.addEventListener('DOMContentLoaded', () => {
+    const createEventForm = document.querySelector('#event-form');
+    createEventForm.addEventListener("submit", e => createEventFormHandler(e));
+
+    const createParticipantForm = document.querySelector('#participant-form')
+    createParticipantForm.addEventListener("submit", e => createParticipantFormHandler(e));
+
+    getEvents();
+})
+
+// get event
+async function getEvents() {
+    fetch(eventEndPoint)
+    .then(response => response.json())
+    .then(events => {
+        events.data.forEach(event => {
+            const newEvent = new Event(event)
+            newEvent.renderEvent();
+        })
+    })
+}
+
+// create event
+function createEventFormHandler(e) {
+    e.preventDefault()
+    const eventNameInput = document.querySelector('#input-event-name').value
+    const eventDescriptionInput = document.querySelector('input-event-description').value
+    postEvent(eventNameInput, eventDescriptionInput)
+}
+
+// post event
+function postEvent(data){
+    fetch(eventEndPoint, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({data})
+    })
+    .then(response => response.json())
+    .then(event => {
+        const newEvent = new Event(event.data)
+        newEvent.renderEvent();
+    })
+    .catch(error => { alert(error.message) })
+}
+
+// delete event
+function deleteEvent(e){
+    fetch(eventEndPoint + `/${e.target.id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
+    })
+    .then(function(resp){
+        if(resp.status = 204)
+            location.reload();
+        else
+            throw new Error(resp.message)
+            console.log(resp.status)
+    })
+    .catch(error => {alert(error.message)})
+}
+
+//create participant
+function createParticipantFormHandler(e) {
+    e.preventDefault()
+    const fullNameInput = document.querySelector('#full-name').value
+    const emailInput = document.querySelector('#email').value
+    const phoneNumberInput = document.querySelector('#phone-number').value
+    const eventId = document.querySelector('#event-list').value
+    postParticipant(fullNameInput, emailInput, phoneNumberInput, eventId)
+}
+
+// post participant
+function postParticipant (full_name, email, phone_number, event_id){
+    const participantObject = {full_name, email, phone_number, event_id}
+
+    fetch(studentsEndPoint, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(participantObject)
+    })
+    .then(resp => resp.json())
+    .then(participant => {
+        let newParticipant = new Participant(participant.data)
+        newParticipant.renderParticipant()
+        // location.reload()
+    })
+    .catch(error => {alert(error.message)})
+}
+
+// delete participant
+function deleteParticipant(e){
+    fetch(participantEndPoint + `/${e.target.id}`, {
+        method: "DELETE",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        }
+    })
+    .then(function(resp){
+        if(resp.status = 204)
+            location.reload();
+        else
+            throw new Error(resp.message)
+            console.log(resp.status)
+    })
+    .catch(error => {alert(error.message)})
 }
