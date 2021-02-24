6b3d915791108c3387cddcbdce9a500ca2c9d4ba
LeonSKennedy-ResidentEvil6 <yiboli.coding@gmail.com>
2021-02-23 22:28:32 -0800

    migrate event participant data & seed data with adding resources routes

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,13 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_28_184626) do
+ActiveRecord::Schema.define(version: 2021_02_24_061834) do
 
   create_table "courses", force: :cascade do |t|
     t.string "name"
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "participants", force: :cascade do |t|
+    t.string "full_name"
+    t.string "email"
+    t.integer "phone_number"
+    t.integer "event_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id"], name: "index_participants_on_event_id"
   end
 
   create_table "students", force: :cascade do |t|
@@ -29,5 +46,6 @@ ActiveRecord::Schema.define(version: 2020_09_28_184626) do
     t.index ["course_id"], name: "index_students_on_course_id"
   end
 
+  add_foreign_key "participants", "events"
   add_foreign_key "students", "courses"
 end
